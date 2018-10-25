0477915eea633e5e86845b1e3cb1846d29b5d8bc
Evohmike <evohmik@gmail.com>
2018-10-25 15:42:26 +0300

    created profile and post class

--- arena/models.py
+++ arena/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+# from django.db import IntegrityError
 
-# Create your models here.
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+ 
+
+class Posts(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  title = models.CharField(max_length = 65)
+  question = models.TextField(max_length=300)
+  author_name = models.CharField(max_length=100)
+  date_written = models.DateField(auto_now=True)
+        
+  def __str__(self):
+      return self.title
