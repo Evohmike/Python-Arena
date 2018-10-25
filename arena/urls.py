90f1ced39a74e85a8b9cea8d64736a39f0f0d745
Evohmike <evohmik@gmail.com>
2018-10-25 10:01:02 +0300

    created urls

---
+++ arena/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    # url('^$',views.home,name = 'home'),
+    url('^$',views.welcome,name='Welcome')
+    
+]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
